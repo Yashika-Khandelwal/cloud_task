174932ebdf1d0c18f0a689e76f050153f8988a1c
Yashika-Khandelwal <64484309+Yashika-Khandelwal@users.noreply.github.com>
2020-06-13 18:42:18 +0530

    fourth

--- index.php
+++ index.php
@@ -1,8 +1,11 @@
 <body bgcolor='#e1e1e1'>
 <pre>
+<html>
+<body>
 <? php
 print "Welcome to Yashika Khandelwal's page" 
-<img src='https://buckettask1.s3.ap-south-1.amazonaws.com/dog.916.jpg' , width='499', height='392'/>
- 
-?>
+<img src='https://s3-bucket-task1.s3.ap-south-1.amazonaws.com/dog.jpg' , width='499', height='392'/>
+ ?>
+</body>
+<html>
 </pre>
