b1e07c2454a29463d0602727a14957031fe6766c
Yashika-Khandelwal <64484309+Yashika-Khandelwal@users.noreply.github.com>
2020-06-13 20:03:29 +0530

    final commit

--- index.php
+++ index.php
@@ -1,10 +1,9 @@
-<body bgcolor='#e1e1e1'>
+Welcome to Yashika Khandelwal's page
 <pre>
 <html>
 <body>
-<? php
-print "Welcome to Yashika Khandelwal's page" 
-<img src='https://s3-bucket-task1.s3.ap-south-1.amazonaws.com/dog.jpg' , width='499', height='392'/>
+<?php
+echo "<img src='https://s3-bucket-task1.s3.ap-south-1.amazonaws.com/dog.jpg' , width='499', height='392' />";
 ?>
 </body>
 </html>
