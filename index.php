3fcff7142298a52b2386a9de8fc97ff7f025d829
Yashika-Khandelwal <64484309+Yashika-Khandelwal@users.noreply.github.com>
2020-06-15 14:43:52 +0530

    final

--- index.php
+++ index.php
@@ -1,10 +1,9 @@
-Welcome to Tanmay Sharma's page.
-There are my friends Shubh (Extreme Left), Yashika (mid), Shivansh (Extreme Right).
+Welcome to Yashika Khandelwal's page.
 <pre>
 <html>
 <body>
 <?php
-echo "<img src='https://s3-bucket-task1.s3.ap-south-1.amazonaws.com/tanmay_friends.jpg' , width='499', height='392' />";
+echo "<img src='https://s3-bucket-task1.s3.ap-south-1.amazonaws.com/dog.jpg' , width='499', height='392' />";
 ?>
 </body>
 </html>
